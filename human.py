4014135a2995e8bf0d7ec00cc033215507044dc0
Дмитрий Акимов <mythrill4d@gmail.com>
2023-10-18 10:00:59 +0300

    Добавил классы Human, Male, Female

---
+++ human.py
@@ -0,0 +1,43 @@
+class Human:
+    """Представление человека"""
+    def __init__(self, name, sex='М'):
+        self.name = name
+        self.sex = sex
+        self.is_married = False
+        self.spouse = None
+
+    def says_hello(self):
+        print(f'{self.name} приветствует тебя!')
+
+    def marry(self, other):
+        if self.is_married:
+            print(f'{self.name} уже в браке!')
+            # return
+        else:
+            self.is_married = True
+            self.spouse = other  # Атрибут надо создавать при инициализации
+            other.is_married = True
+            other.spouse = self
+            print(f'Поздравляем молодоженов: {person1.name} и {person2.name}!')
+            # можно объединить с атрибутом is_married
+
+
+class Male(Human):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Female(Human):
+    def __init__(self, name):
+        super().__init__(name, sex='Ж')
+
+
+print(f'{__name__=}')
+
+if __name__ == '__main__':
+    person1 = Male('Гоша')
+    person2 = Female('Маша')
+
+    person1.marry(person2)
+    print(f'{person1.name=} {person1.sex=}')
+    print(f'{person2.name=} {person2.sex=}')
