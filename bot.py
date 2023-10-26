8dfaec6938c99e3630010416d9211724df61a99e
Dmitrii Akimov <akimov@1060.ru>
2023-10-26 09:14:50 +0300

    Добавил установку таймера

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, ParseMode
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove  # Обычная текстовая клавиатура
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup  # Инлайн-клавиатура
 from telegram.ext import Updater, Dispatcher
@@ -11,7 +11,7 @@ import logging
 from key import TOKEN
 
 logging.basicConfig(
-    format='%(asctime)s - %(levelname)s - %(name)s: %(message)s',
+    format='%(asctime)s | %(levelname)s | %(name)s: %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
@@ -29,12 +29,14 @@ def main():
     start_handler = CommandHandler(['start', 'help'], do_start)
     keyboard_handler = CommandHandler('keyboard', do_keyboard)
     inline_keyboard_handler = CommandHandler('inline_keyboard', do_inline_keyboard)
+    set_timer_handler = CommandHandler('set', set_timer)
     callback_handler = CallbackQueryHandler(keyboard_react)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(keyboard_handler)
     dispatcher.add_handler(inline_keyboard_handler)
     dispatcher.add_handler(callback_handler)
+    dispatcher.add_handler(set_timer_handler)
     dispatcher.add_handler(echo_handler)
 
     updater.start_polling()
@@ -60,18 +62,31 @@ def do_echo(update: Update, context: CallbackContext):
 def do_start(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     logger.info(f'{user_id=} вызвал функцию start')
-    text = ['Приветствую тебя, кожаный мешок!',
-            f'Твой {user_id=}',
-            'Я знаю команды:',
-            '/start',
-            '/keyboard',
-            '/inline_keyboard']
+    text = [
+        'Приветствую тебя, кожаный мешок!',
+        f'Твой <b>{user_id=}</b>',
+        'Я знаю команды:',
+        '/start',
+        '/keyboard',
+        '/inline_keyboard',
+        '/set',
+        'html',
+        '<b>Сам жирный</b>',
+        '<i>Курсив</i>',
+        '<code>код</code>',
+        '<s>перечеркнутый</s>',
+        '<u>подчеркнутый</u>',
+        '<pre language="python">print("Hello, World!")</pre>',
+        '<a href="https://technodzen.com/nauka/'
+        'sozdan-magnitnyy-gel-zazhivlyayuschiy-rany-u-diabetikov-v-tri-raza-bystree?'
+        'utm_source=yxnews&utm_medium=desktop">Сайт</a>'
+    ]
     text = '\n'.join(text)
     # context.bot.send_message(
     #     user_id,
     #     text
     # )
-    update.message.reply_text(text)
+    update.message.reply_text(text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
 
 
 def do_keyboard(update: Update, context: CallbackContext):
@@ -131,6 +146,15 @@ def keyboard_react(update: Update, context: CallbackContext):
         text,
         reply_markup=keyboard
     )
+
+
+def set_timer(update: Update, context: CallbackContext):
+    context.job_queue.run_repeating(show_seconds, 10)
+
+
+def show_seconds(context: CallbackContext):
+    user_id = 262388958
+    context.bot.send_message(user_id, 'прошло 10 секунд')
 
 
 if __name__ == '__main__':
