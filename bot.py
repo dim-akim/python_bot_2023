c9278ca411a432f5fd66f820b40887d1674a5248
Дмитрий Акимов <akimov@1060.ru>
2023-10-26 14:15:39 +0300

    Теперь таймер выводит только одно сообщение и изменяет его

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from telegram.ext import CallbackContext
 from telegram.ext import Filters
 
 import logging
+import datetime
 
 from key import TOKEN
 
@@ -30,6 +31,7 @@ def main():
     keyboard_handler = CommandHandler('keyboard', do_keyboard)
     inline_keyboard_handler = CommandHandler('inline_keyboard', do_inline_keyboard)
     set_timer_handler = CommandHandler('set', set_timer)
+    stop_timer_handler = CommandHandler('stop', stop_timer)
     callback_handler = CallbackQueryHandler(keyboard_react)
 
     dispatcher.add_handler(start_handler)
@@ -37,6 +39,7 @@ def main():
     dispatcher.add_handler(inline_keyboard_handler)
     dispatcher.add_handler(callback_handler)
     dispatcher.add_handler(set_timer_handler)
+    dispatcher.add_handler(stop_timer_handler)
     dispatcher.add_handler(echo_handler)
 
     updater.start_polling()
@@ -149,12 +152,32 @@ def keyboard_react(update: Update, context: CallbackContext):
 
 
 def set_timer(update: Update, context: CallbackContext):
-    context.job_queue.run_repeating(show_seconds, 10)
+    user_id = update.effective_user.id
+    context.bot_data['user_id'] = user_id
+    context.bot_data['timer'] = datetime.datetime.now()
+    context.bot_data['timer_job'] = context.job_queue.run_repeating(show_seconds, 1)
 
 
 def show_seconds(context: CallbackContext):
-    user_id = 262388958
-    context.bot.send_message(user_id, 'прошло 10 секунд')
+    logger.info(f'{context.job_queue.jobs()}')
+    message_id = context.bot_data.get('message_id', None)
+    user_id = context.bot_data['user_id']
+    timer = datetime.datetime.now() - context.bot_data['timer']
+    timer = timer.seconds
+    text = f'прошло {timer} секунд'
+    text += '\nнажмите /stop чтобы остановить таймер'
+    if not message_id:
+        message = context.bot.send_message(user_id, text)
+        context.bot_data['message_id'] = message.message_id
+    else:
+        context.bot.edit_message_text(text, chat_id=user_id, message_id=message_id)
+
+
+def stop_timer(update: Update, context: CallbackContext):
+    logger.info(f'Запущена функция delete_timer')
+    timer = datetime.datetime.now() - context.bot_data['timer']
+    context.bot_data['timer_job'].schedule_removal()
+    update.message.reply_text(f'Таймер остановлен. Прошло {timer} секунд')
 
 
 if __name__ == '__main__':
